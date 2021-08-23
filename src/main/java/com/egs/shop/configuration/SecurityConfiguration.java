02d436d48293713e4eb8cad9f654399dc45a1e1d
Mohammad <m.nazarizadeh@gmail.com>
2021-08-23 14:07:07 +0430

    security configuration

--- src/main/java/com/egs/shop/configuration/SecurityConfiguration.java
+++ src/main/java/com/egs/shop/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import com.egs.shop.security.jwt.JWTConfigurer;
 import com.egs.shop.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @RequiredArgsConstructor
